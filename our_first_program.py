d806a92c5f4812e410c0297ff30f4a2da084d8c5
Lula Robertson <lrobertson4@luc.edu>
2025-05-20 17:30:23 +0000

    hello students

--- our_first_program.py
+++ our_first_program.py
@@ -1,1 +1,18 @@
-print("Hello COMP 170!")
+
+def greeting(name):
+    print("Hello COMP 170")
+    print(f"Hello {name}")
+
+# Pattern:
+# "Hello" first_name", welcome to "course_code"
+
+def personalized_greeting(name, course_code):
+    print(f"Hello {name}, welcome to {course_code}.")
+
+personalized_greeting("lula", "COMP 170")
+
+students = ["Omar", "Lula", "Enrique", "Arhub"]
+your_course = "COMP 170"
+
+for name in students:
+    personalized_greeting(name, your_course)
