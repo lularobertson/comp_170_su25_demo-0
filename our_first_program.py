60230b691ea099fc86352d2ef8b93bb7618fb8d2
Lula Robertson <lrobertson4@luc.edu>
2025-05-21 17:22:48 +0000

    indexing?

--- our_first_program.py
+++ our_first_program.py
@@ -7,12 +7,25 @@ def greeting(name):
 # "Hello" first_name", welcome to "course_code"
 
 def personalized_greeting(name, course_code):
+
+#parameters are "name" and "course_code"
+
     print(f"Hello {name}, welcome to {course_code}.")
 
 personalized_greeting("lula", "COMP 170")
 
+#now we can create a list of the students.
+
 students = ["Omar", "Lula", "Enrique", "Arhub"]
 your_course = "COMP 170"
+#this is a loop.
 
 for name in students:
     personalized_greeting(name, your_course)
+
+#for name in students:
+# personalized_greeting(name, your_course)
+
+for index in range(len(students)):
+    personalized_greeting(students[index], your_course)
+    print(index)
